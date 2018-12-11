04d12320f0d438ff7f38386c673202138d06c4f4
wangyuansxya <wangyuansxya@163.com>
2018-12-11 14:31:19 +0800

    层序遍历

--- src/com/tree/InorderTraversal.java
+++ src/com/tree/InorderTraversal.java
@@ -1,8 +1,8 @@
 package com.tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class InorderTraversal {
 
@@ -41,14 +41,59 @@ public class InorderTraversal {
 
     public static void main(String[] args) {
 
-            TreeNode root = new TreeNode(1);
-            root.right = new TreeNode(2);
-            root.right.left = new TreeNode(3);
-            root.left = new TreeNode(4);
-            List<Integer> res = inorderTraversal(root);
-            for (Integer i : res) {
-                System.out.println("args = [" + i + "]");
-            }
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        root.left = new TreeNode(4);
+        /*List<Integer> res = inorderTraversal(root);
+        for (Integer i : res) {
+            System.out.println("args = [" + i + "]");
+        }*/
+        /*List<List<Integer>> result = new ArrayList<>();
+        int index = 0;
 
+        while (!queue.isEmpty()) {
+
+
+
+        }
+        for (TreeNode i : queue) {
+            System.out.println("args = [" + i.val + "]");
+        }*/
+        List<List<Integer>> res = levelOrder(root);
+
+        for (List<Integer> item: res) {
+            if(item == null) continue;
+            System.out.println("item = [" + item + "]");
+            /*for (int i : item) {
+                System.out.println("i = [" + i + "]");
+            }*/
+        }
+    }
+
+
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+        if(root == null) return null;
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<TreeNode> queue =  new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> l = new ArrayList<>(size);
+            for (int i = 0 ; i < size; i++) {
+                root = queue.poll();
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                l.add(root.val);
+            }
+            result.add(l);
+        }
+        return result;
     }
 }
